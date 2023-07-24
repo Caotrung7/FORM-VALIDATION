0754b531955908c04c799835d96626010d09c35f
caotrung <caotrung1709@gmail.com>
2023-07-24 22:52:00 +0700

    -Form validation 2_create function for attribute in html

--- FORM VALIDATION 1/js/validator.js
+++ FORM VALIDATION 1/js/validator.js
@@ -9,8 +9,25 @@ function Validator(formSelector) {
 
   var validatorRules = {
     required: function (value) {
+      return value ? undefined : 'Vui lòng nhập trường này';
+    },
 
-    }
+    email: function (value) {
+      var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; //Check email type
+      return regex.test(value) ? undefined : 'Vui lòng nhập email';
+    },
+
+    min: function (min) {
+      return function (value) {
+        return value.length >= min ? undefined : `Vui lòng nhập ít nhất ${min} ký tự`;
+      }
+    },
+
+    max: function (max) {
+      return function (value) {
+        return value.length <= max ? undefined : `Vui lòng nhập tối đa ${max} ký tự`;
+      }
+    },
   }
 
   //Lay ra form element trong DOM theo 'formSelector'
@@ -20,7 +37,28 @@ function Validator(formSelector) {
   if (formElement) {
     var inputs = formElement.querySelectorAll('[name][rules]');
     for (var input of inputs) {
-      formRules[input.name] = input.getAttribute('rules');
+      var rules = input.getAttribute('rules').split('|');
+      for (var rule of rules) {
+
+        var ruleInfo;
+        var isRuleHasValue = rule.includes(':');
+
+        if (isRuleHasValue) {
+          ruleInfo = rule.split(':');
+          rule = ruleInfo[0];
+        }
+        var ruleFunc = validatorRules[rule];
+
+        if (isRuleHasValue) {
+          ruleFunc = ruleFunc(ruleInfo[1]);
+        }
+
+        if (Array.isArray(formRules[input.name])) {
+          formRules[input.name].push(ruleFunc);
+        } else {
+          formRules[input.name] = [ruleFunc];
+        }
+      }
     }
     console.log(formRules);
   }
